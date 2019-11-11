54dc027386d6d70594f65da5095bda737cdbfcf6
BrewhogIT <ssslll69@gmail.com>
2019-11-11 08:39:09 +0500

    start new project

---
+++ app/src/main/java/com/brewhog/android/tradepractice/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package com.brewhog.android.tradepractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
