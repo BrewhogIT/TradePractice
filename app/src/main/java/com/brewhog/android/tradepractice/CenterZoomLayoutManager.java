c13ac2fb0956117c55559ebec7ec3ba02cdd497b
BrewhogIT <ssslll69@gmail.com>
2020-02-06 17:00:36 +0500

    change lesson kind topics transparency when scroll

--- app/src/main/java/com/brewhog/android/tradepractice/CenterZoomLayoutManager.java
+++ app/src/main/java/com/brewhog/android/tradepractice/CenterZoomLayoutManager.java
@@ -1,15 +1,19 @@
 package com.brewhog.android.tradepractice;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CenterZoomLayoutManager extends LinearLayoutManager {
 
+    private final float mShrinkAlpha = 0.9f;
     private final float mShrinkAmount = 0.15f;
     private final float mShrinkDistance = 0.9f;
+    private static final String TAG = "CenterZoomLayoutManager";
 
     public CenterZoomLayoutManager(Context context) {
         super(context);
@@ -30,14 +34,21 @@ public class CenterZoomLayoutManager extends LinearLayoutManager {
             float d1 = mShrinkDistance * midpoint;
             float s0 = 1.f;
             float s1 = 1.f - mShrinkAmount;
+            float a1 = 1.f - mShrinkAlpha;
+
             for (int i = 0; i < getChildCount(); i++){
                 View child = getChildAt(i);
+                TextView topic = child.findViewById(R.id.lesson_kind_topic);
+
                 float childMidpoint =
                         (getDecoratedLeft(child) + getDecoratedRight(child)) / 2.f;
                 float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
                 float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
                 child.setScaleX(scale);
                 child.setScaleY(scale);
+
+                float alpha = s0 + (a1 - s0) * (d - d0) / (d1 - d0);
+                topic.setAlpha(alpha);
             }
             return scrolled;
         }else {
@@ -56,14 +67,21 @@ public class CenterZoomLayoutManager extends LinearLayoutManager {
             float d1 = mShrinkDistance * midpoint;
             float s0 = 1.f;
             float s1 = 1.f - mShrinkAmount;
+            float a1 = 1.f - mShrinkAlpha;
+
             for (int i = 0; i < getChildCount(); i++){
                 View child = getChildAt(i);
+                TextView topic = child.findViewById(R.id.lesson_kind_topic);
+
                 float childMidpoint =
                         (getDecoratedBottom(child) + getDecoratedTop(child)) / 2.f;
                 float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
                 float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
                 child.setScaleX(scale);
                 child.setScaleY(scale);
+
+                float alpha = s0 + (a1 - s0) * (d - d0) / (d1 - d0);
+                topic.setAlpha(alpha);
             }
 
             return scrolled;
